e1f8a5344e18a0ac5ba7c77bc588b5200af24f26
Ojas Kale <ojasok@gmail.com>
2020-02-04 13:33:47 +0530

    reverting db

--- django_ci_cd/settings.py
+++ django_ci_cd/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'django_ci_cd.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'pollsadmin@postgres-server-2',
-        'PASSWORD': 'P@1579vV0r)',
-        'HOST': 'postgres-server-2.postgres.database.azure.com',
-        'PORT': 5432,
-        'OPTIONS': {'sslmode': 'require'},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'postgres',
+    #     'USER': 'pollsadmin@postgres-server-2',
+    #     'PASSWORD': 'P@1579vV0r)',
+    #     'HOST': 'postgres-server-2.postgres.database.azure.com',
+    #     'PORT': 5432,
+    #     'OPTIONS': {'sslmode': 'require'},
+    # }
 }
 
 
