bec672088aafc3e8b2a3d135ee585f10065f467f
Ojas Kale <ojasok@gmail.com>
2020-02-05 19:44:24 +0530

    added logs

--- django_ci_cd/settings.py
+++ django_ci_cd/settings.py
@@ -116,7 +116,7 @@ LOGGING = {
         'logstash': {
             'level': 'DEBUG',
             'class': 'logstash.TCPLogstashHandler',
-            'host': 'sample-logstash',
+            'host': 'localhost',
             'port': 5959, # Default value: 5959
             'version': 1, # Version of logstash event schema. Default value: 0 (for backward compatibility of the library)
             'message_type': 'django',  # 'type' field in logstash message. Default value: 'logstash'.
@@ -133,6 +133,47 @@ LOGGING = {
     }
 }
 
+# LOGGING ={
+#     'version':1,
+#     'disable_existing_loggers':False,
+#     'formatters':{
+#         'simple':{
+#             'format':'velname)s %(message)s'
+#         },
+
+#     },
+#     'handlers':{
+#         'console':{
+#             'level':'DEBUG',
+#             'class':'logging.StreamHandler',
+#             'formatter':'simple'
+#         },
+#         'logstash':{
+#             'level':'DEBUG',
+#             'class':'logstash.TCPLogstashHandler',
+#             'host':'logstash',
+#             'port':5000,
+#             'version':1,
+#             'message_type':'django',
+#             #            'type'            logstash .            'fqdn':False,
+#             'tags':[
+#                 'django'
+#             ],
+#             #  .
+#         },
+
+#     },
+#     'loggers':{
+#         'django.request':{
+#             'handlers':[
+#                 'logstash'
+#             ],
+#             'level':'INFO',
+#             'propagate':True,
+
+#         },
+#     }
+# }
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
-
+import logging 
+logger = logging.getLogger(__name__)
+from random import randrange
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index app")
+	levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+	num = randrange(len(levels))
+
+	if num == 0:
+		logger.debug("This is a debug")	
+	elif num == 1:	
+		logger.info("This is a info")	
+	elif num == 2:
+		logger.warn("This is a warning")		
+	elif num == 3:
+		logger.error("This is an error")
+	elif num == 4:
+		logger.critical("This is an critical")					
+
+	return HttpResponse("Hello, world. You're at the polls index app")
